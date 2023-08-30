60f8f1fe90ce24df649b6f64862c77a4f7af9fc8
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2023-08-30 00:56:20 -0300

    Add files via upload

---
+++ HotelBookingAPI/Controllers/HotelBookingController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Data;
+using System.Linq;
+
+namespace HotelBookingAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public HotelBookingController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public JsonResult CreateEdit(HotelBooking booking)
+        {
+            if (booking.Id == 0)
+            {
+                _context.Bookings.Add(booking);
+            }
+            else
+            {
+                var bookingInDb = _context.Bookings.Find(booking.Id);
+                if (bookingInDb == null)
+                {
+                    return new JsonResult(NotFound());
+                }
+                bookingInDb.RoomNumber = booking.RoomNumber;
+                bookingInDb.ClientsName = booking.ClientsName;
+                bookingInDb.lastName = booking.lastName;
+                bookingInDb.age = booking.age;
+                bookingInDb.startDate = booking.startDate;
+                bookingInDb.endDate = booking.endDate;
+                bookingInDb.phoneNumber = booking.phoneNumber;
+                bookingInDb.houseId = booking.houseId;
+                bookingInDb.discountCode = booking.discountCode;
+            }
+
+            _context.SaveChanges();
+            return new JsonResult(Ok(booking));
+        }
+
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            var booking = _context.Bookings.Find(id);
+            if (booking != null)
+            {
+                return new JsonResult(Ok(booking));
+            }
+            else
+            {
+                return new JsonResult(NotFound());
+            }
+        }
+
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            var result = _context.Bookings.ToList();
+            if (result == null)
+            {
+                return new JsonResult(NotFound());
+            }
+            return new JsonResult(Ok(result));
+        }
+
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            var booking = _context.Bookings.Find(id);
+            if (booking == null)
+            {
+                return new JsonResult(NotFound());
+            }
+            _context.Bookings.Remove(booking);
+            _context.SaveChanges();
+            return new JsonResult(NoContent());
+        }
+    }
+}
---
+++ HotelBookingAPI/Models/HotelBooking.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public string RoomNumber { get; set; }
+
+        [MinLength(2), MaxLength(50)]
+        public string ClientsName { get; set; }
+
+        [MinLength(2), MaxLength(50)]
+        public string lastName { get; set; }
+
+        [Range(18,100)]
+        public int age { get; set; }
+        public int startDate { get; set; }
+        public int endDate { get; set; }
+
+        [MinLength(9), MaxLength(20)]
+        public string phoneNumber { get; set; }
+
+        [MinLength(6), MaxLength(15)]
+        public string houseId { get; set; }
+
+        [MinLength(8), MaxLength(8)]
+        public string discountCode { get; set; }
+
+    }
+}
